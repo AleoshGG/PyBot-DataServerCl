a6bf04afb1d1ecbe19197b5ff03a43b31a5271a3
LimbergDV <limbergdelvalle@gmail.com>
2025-07-15 13:56:13 -0600

    [MODIFIED] modicando el cu y el controlador de admins

--- admin/application/useCases/GetAllids_useCase.py
+++ admin/application/useCases/GetAllids_useCase.py
@@ -1,29 +1,11 @@
-from flask import jsonify
-from admin.application.useCases.GetAllids_useCase import GetAllIds
-from admin.infrastructure.adapters.SQLAlchemy import SQLAlchemy
+from dataclasses import dataclass
+from admin.domain.repositories.admin_repository import IAdmin
+from typing import List
 
-class GetAllIdsController:
-    def __init__(self):
-        self.SQLAlchemy = SQLAlchemy()
-        self.use_case = GetAllIds(db=self.SQLAlchemy)
+@dataclass
+class GetAllids:
+    def __init__(self, db:IAdmin):
+        self.db = db
 
-    def getAllIds(self):
-        try:
-            ids = self.use_case.run()
-        except Exception as e:
-            print(f"Error al obtener los IDs: {e}")
-            return jsonify({
-                "status": False,
-                "error": f"Error al obtener los IDs: {e}"
-            }), 500
-
-        return jsonify({
-            "status": True,
-            "data": {
-                "type": "admin",
-                "attributes": {
-                    "ids": ids,
-                    "total": len(ids)
-                }
-            }
-        }), 200
+    def run(self) -> List[str]:
+        return self.db.get_all_ids()    
